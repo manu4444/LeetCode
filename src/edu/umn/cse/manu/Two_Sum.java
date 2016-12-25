0af1bdaeed55e09b175f9710313419662d2d0b27
manu <masters.manuk@gmail.com>
2016-12-24 22:49:01 -0600

    1:Two Sum Solution

---
+++ src/edu/umn/cse/manu/Two_Sum.java
@@ -0,0 +1,31 @@
+package edu.umn.cse.manu;
+
+import java.util.Arrays;
+
+/**
+ * Created by maste on 12/24/2016.
+ */
+public class Two_Sum {
+    public static int[] twoSum(int[] nums, int target) {
+
+        int[] temp = {0,0};
+        int val = 0;
+        int arrLen = nums.length;
+        for(int i=0; i< arrLen; i++){
+            for(int j=i+1; j<arrLen; j++){
+                if(nums[i]+nums[j] == target){
+                    temp[0] = i; temp[1] = j;
+                    return temp;
+                }
+            }
+        }
+
+        return temp;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
