a972089319219a79845b7478403dbb61856d3d8a
manu <masters.manuk@gmail.com>
2016-12-24 22:52:44 -0600

    7: Reverse Integer Solution

---
+++ src/edu/umn/cse/manu/Reverse_Integer.java
@@ -0,0 +1,28 @@
+package edu.umn.cse.manu;
+
+import java.util.Arrays;
+
+/**
+ * Created by maste on 12/24/2016.
+ */
+public class Reverse_Integer {
+     public static int reverse(int x) {
+            long reverse = 0;
+            while( x != 0 ) {
+                reverse = reverse * 10 + x % 10;
+                x = x/10;
+            }
+
+            if(reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+                return 0;
+            } else {
+                return (int) reverse;
+            }
+
+        }
+
+    public static void main(String[] args){
+        int input = 1234;
+        System.out.println(reverse(input));
+    }
+}
