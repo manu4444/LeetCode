9a9d3beb5784adde8875dde3afe97edcface8e37
manu <masters.manuk@gmail.com>
2016-12-25 19:14:13 -0600

    217. Contains Duplicate | Time Improvement

--- src/edu/umn/cse/manu/Contains_Duplicate_217.java
+++ src/edu/umn/cse/manu/Contains_Duplicate_217.java
@@ -1,15 +1,30 @@
 package edu.umn.cse.manu;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by maste on 12/25/2016.
+ *
+ * Use hashset instead of hashmap
  */
 public class Contains_Duplicate_217 {
 
     public static boolean containsDuplicate(int[] nums) {
 
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // Instead you can use hashset as well
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+
+        return false;
+
+        /*HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
                 return true;
@@ -17,11 +32,11 @@ public class Contains_Duplicate_217 {
             map.put(nums[i], i);
         }
 
-        return false;
+        return false;*/
     }
 
     public static void main(String[] args) {
-        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9};
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int maxDistance = 3;
         System.out.println(containsDuplicate(input));
     }
